1a1839d83e349fc626cba0699644f02ba5bc1231
Viner108 <andrukovichl@mail.ru>
2024-04-10 15:22:45 +0300

    Sockets for server

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-console.log("hello");
+// const socketReceiver = io.connect('http://192.168.1.107:8002');
+// const socketSender = io.connect('http://192.168.1.107:8001');
+// socketReceiver.on('message',(data) =>{
+//     console.log('Получено сообщение: ', data);
+// });
+// function sendMessage(message) {
+//     socketSender.emit('message', message);
+// };
